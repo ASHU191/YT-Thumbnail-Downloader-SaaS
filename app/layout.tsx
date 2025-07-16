0107bd85e54d1c96c86a4740707e75ba0bb3774b
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2025-07-17 01:49:03 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Muhammad Arsalan Aftab',
+  description: 'Created with Love',
+  generator: 'Muhammad Arsalan Aftab',
 }
 
 export default function RootLayout({
