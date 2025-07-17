602bbaf111e0f45fe29a3bb407f2869b7d771462
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2025-07-18 00:53:11 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -246,8 +246,8 @@ export default function HomePage() {
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             <div className="md:col-span-2">
               <div className="flex items-center space-x-3 mb-4">
-               <img src="/thumnail-downloader.png?height=40&width=40" alt="YT Tools Logo" className="w-22 h-14 rounded-lg" /
-                <span className="text-xl font-bold">YT Tools</span>
+                <img src="/thumnail-downloader.png?height=40&width=40" alt="YT Tools Logo" className="w-22 h-14 rounded-lg" />
+                <span className="text-xl font-bold"></span>
               </div>
               <p className="text-gray-400 mb-4 max-w-md">{t.home.footer.description}</p>
             </div>
