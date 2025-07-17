a7faf1eb07aea91ddecdbfbedc55f6cefc2f9b11
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2025-07-17 14:33:26 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Muhammad Arsalan Aftab',
+  title: 'YT Media Downloader',
   description: 'Created with Love',
   generator: 'Muhammad Arsalan Aftab',
 }
